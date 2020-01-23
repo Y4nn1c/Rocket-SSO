c9fa05bfce095e9bb1a50140fc36c5068bba5c5e
Yannic <y.schleifer@vectorsoft.de>
2020-01-23 11:42:08 +0100

    added Google-login

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,41 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <h1>HELLO WORLD!</h1>
     <h2>No Dist</h2>
+    <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess" :onFailure="onFailure"></GoogleLogin>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import { GoogleLogin } from "vue-google-login";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    GoogleLogin
+  },
+  data(){
+    return{
+      // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+      params: {
+        client_id: "884184644232-3nu1245erhln6pmc0mfep4n6o9dmh0vh.apps.googleusercontent.com"
+      },
+      // only needed if you want to render the button with the google ui
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true
+      }
+    }
+  },
+  methods: {
+    onSuccess(param){
+      window.console.log("====SUCCESS====", param);
+      window.console.log("====USER PROFILE====", param.getBasicUserProfile());
+    },
+    onFailure(param){
+      window.console.log("====FALURE====", param);
+    }
   }
 };
 </script>
