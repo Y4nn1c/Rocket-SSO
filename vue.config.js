736ad11862340d2712a127e6877f5f48ccd2c1c1
Yannic <y.schleifer@vectorsoft.de>
2020-01-23 10:33:49 +0100

    Initial Commit

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        //prependData: `@import "@/GlobalStyles/GlobalColors.scss";`
+      }
+    }
+  },
+  devServer: {
+    https: false
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/Rocket-SSO/" : "/"
+  // Using only for dev "Access-Control-Allow-Origin"
+  //devServer: {
+  //   proxy: 'http://10.1.4.33:8080',
+  // }
+};
