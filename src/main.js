0ae7dab45d187c86a4e30f2419c7e71643337e95
Yannic <y.schleifer@vectorsoft.de>
2020-01-24 13:28:35 +0100

    added g-sign-in in main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import GSignInButton from "vue-google-signin-button";
 Vue.config.productionTip = false;
-
+Vue.use(GSignInButton);
 new Vue({
   render: h => h(App)
 }).$mount("#app");
