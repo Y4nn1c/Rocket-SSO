126cdc082b17aa811a886348863836ef69835b1b
Yannic <y.schleifer@vectorsoft.de>
2020-01-24 13:37:19 +0100

    test script added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import GSignInButton from "vue-google-signin-button";
+import "https://apis.google.com/js/api:client.js"
 Vue.config.productionTip = false;
 Vue.use(GSignInButton);
 new Vue({
