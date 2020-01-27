e297ae5673868a1f79f5579737bfcc148ec87825
Yannic <y.schleifer@vectorsoft.de>
2020-01-27 10:51:57 +0100

    a

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,9 @@
       <input v-model="id_token" />
       <p>idToken: {{ id_token }}</p>
       <p>valid: {{ valid }}</p>
+      <br />
       <button v-on:click="validateToken">Validate</button>
+      <br />
       Is the token valid? YES: Is it already in our user DB? Yes: exchange it
       for users respective Rocket Session token. No: Generate a new Rocket
       Session Token for the user and save it. No: Cant login, No valid token.
